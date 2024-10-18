9bfd1b4108953187b844641c57bf04163bb4f086
tsion-hailu <hailutsion16@gmail.com>
2024-10-18 21:18:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 // const port = 5500;
-
+const port= 5500
 const cors = require('cors')
 app.use(cors());
 const dotenv = require("dotenv");
